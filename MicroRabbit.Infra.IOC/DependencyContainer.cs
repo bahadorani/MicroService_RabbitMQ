556c6c81cbf6de93f68b25c899cdc99aa011f64e
bahareh.bahadorani@yahoo.com <bahareh.bahadorani@yahoo.com>
2022-05-08 14:34:25 +0430

    Initial Commit of Infra Instructure Dependency

---
+++ MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Infra.Bus;
+using MicroService_RabbitMQ.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IOC
+{
+   public  class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
